4d880c56064d198e1184fdd743de0fba3ffdadcf
Arne Bjune <arne@dahlbjune.no>
2013-03-16 23:08:56 -0700

    new files

---
+++ src/edu/ucsb/aggregator/FaceBookPost.java
@@ -0,0 +1,63 @@
+package edu.ucsb.aggregator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import android.util.Log;
+
+public class FaceBookPost extends Update {
+
+	private final static String TAG = "FaceBookPost";
+
+	public static Update getPost(JSONObject jsonObject) {
+		String type = "";
+		try {
+			type = jsonObject.getString("type");
+		} catch (JSONException e) {
+			Log.i(TAG, e.toString());
+		}
+		if (type.equals("status") || type.equals("photo")) {
+			return new FaceBookPost(jsonObject, type);
+		}
+		return null;
+	}
+
+	public FaceBookPost(JSONObject jsonObject, String type) {
+		try {
+			setUserId(jsonObject.getJSONObject("from").getString("id"));
+		} catch (JSONException e1) {
+			Log.i(TAG, e1.toString());
+		}
+		try {
+			setTitle(jsonObject.getJSONObject("from").getString("name"));
+		} catch (JSONException e1) {
+			Log.i(TAG, e1.toString());
+		}
+		try {
+			setCountCommet(jsonObject.getJSONObject("comments").getInt("count"));
+		} catch (JSONException e1) {
+			Log.i(TAG, e1.toString());
+		}
+		try{
+			setCountLikes(jsonObject.getJSONObject("likes").getInt("count"));
+		} catch (JSONException e1) {
+			Log.i(TAG, e1.toString());
+		}
+		try {
+			setMessage(jsonObject.getString("message"));
+		} catch (JSONException e1) {
+			Log.i(TAG, e1.toString());
+		}
+		try {
+			setUpdatedTime(jsonObject.getString("created_time"));
+		} catch (JSONException e1) {
+			Log.i(TAG, e1.toString());
+		}
+		try{
+			setImageUrl(jsonObject.getString("picture"));
+		} catch (JSONException e1) {
+			Log.i(TAG, e1.toString());
+		}
+	}
+}
